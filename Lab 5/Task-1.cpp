ba663fe453e4daaae9064b59a395aeeabacaff68
Julfiqure <113965603+Julfiqure@users.noreply.github.com>
2022-10-30 23:08:05 +0600

    Create Task-1.cpp

--- Lab 5/Task-1.cpp
+++ Lab 5/Task-1.cpp
@@ -19,7 +19,7 @@ int main()
     }
     cout<<endl<<endl;
     cout<<"After Bubble Sorting:";
-    for(int i=0;i<(n-1);i++)\
+    for(int i=0;i<(n-1);i++)
     {
         for(int j=0;j<(n-i-1);j++)
         {
